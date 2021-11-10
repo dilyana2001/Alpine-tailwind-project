b23552aafb62bdcdfa48445c4b348f3a782a595f
dilyana2001 <dilyana2001@gmail.com>
2021-11-10 16:22:16 +0200

    Validation added

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,36 +1,40 @@
 function contactForm() {
-    let error = '';
     return {
-        data: {
+        formData: {
             email: '',
             question: '',
             message: ''
         },
-
+        error: '',
         submitDataHandler() {
-            if (!this.data.email || !this.data.question || !this.data.message) {
-                error = 'All fields are required!';
-                console.log(error);
-                return error;
+            this.error = '';
+            if (!this.formData.email || !this.formData.question || !this.formData.message) {
+                this.error = 'All fields are required!'
+                return;
             }
-            if (!this.data.email.match(/[a-z\d]+[.]?[_]?[-]?[a-z\d]+@{1}[a-z]+[-]?[a-z]+[.]?[a-z]+[.][a-z]{2,}/g)) {
-                error = 'Please enter a correct email address!';
-                console.log(error)
-                return error;
+            if (!this.formData.email.match(/[a-z\d]+[.]?[_]?[-]?[a-z\d]+@{1}[a-z]+[-]?[a-z]+[.]?[a-z]+[.][a-z]+/g)) {
+                this.error = 'Please enter a correct email address!'
+                return;
             }
-            if (this.data.question.length < 5 || this.data.message.length < 5) {
-                error = 'Question and message need to be at least five symbols!';
-                console.log(error)
-                return error;
+            if (this.formData.question.length < 5 || this.formData.message.length < 5) {
+                this.error = 'Question and message need to be at least five symbols!'
+                return;
             }
 
-            fetch('/contact', {
+            return fetch('/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(this.data)
+                body: JSON.stringify(this.formData)
             })
+                .then(() => {
+                    this.formData.email = '';
+                    this.formData.question = '';
+                    this.formData.message = '';
+                    alert('Thank you! Your question is sucessfully submitted!')
+                })
+                .catch(() => this.error)
         }
     }
 }
