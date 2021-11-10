8afbf7de78b451503ce8476e50cc4ff8cf34c644
dilyana2001 <dilyana2001@gmail.com>
2021-11-10 18:14:49 +0200

    Partial header implemented

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -9,15 +9,15 @@ function contactForm() {
         submitDataHandler() {
             this.error = '';
             if (!this.formData.email || !this.formData.question || !this.formData.message) {
-                this.error = 'All fields are required!'
+                this.error = 'All fields are required!';
                 return;
             }
             if (!this.formData.email.match(/[a-z\d]+[.]?[_]?[-]?[a-z\d]+@{1}[a-z]+[-]?[a-z]+[.]?[a-z]+[.][a-z]+/g)) {
-                this.error = 'Please enter a correct email address!'
+                this.error = 'Please enter a correct email address!';
                 return;
             }
             if (this.formData.question.length < 5 || this.formData.message.length < 5) {
-                this.error = 'Question and message need to be at least five symbols!'
+                this.error = 'Question and message need to be at least five symbols!';
                 return;
             }
 
@@ -32,9 +32,9 @@ function contactForm() {
                     this.formData.email = '';
                     this.formData.question = '';
                     this.formData.message = '';
-                    alert('Thank you! Your question is sucessfully submitted!')
+                    alert('Thank you! Your question is sucessfully submitted!');
                 })
-                .catch(() => this.error)
+                .catch(() => this.error);
         }
     }
 }
