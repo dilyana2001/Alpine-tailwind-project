8295dd95303e036d7154837f7fbe5e787f5966c4
dilyana2001 <dilyana2001@gmail.com>
2021-11-11 09:59:36 +0200

    Changes on Css

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -9,15 +9,15 @@ function contactForm() {
         submitDataHandler() {
             this.error = '';
             if (!this.formData.email || !this.formData.question || !this.formData.message) {
-                this.error = 'All fields are required!';
+                this.error = 'Tous les champs sont requis!';
                 return;
             }
             if (!this.formData.email.match(/[a-z\d]+[.]?[_]?[-]?[a-z\d]+@{1}[a-z]+[-]?[a-z]+[.]?[a-z]+[.][a-z]+/g)) {
-                this.error = 'Please enter a correct email address!';
+                this.error = 'S\'il vous plaît, mettez une adresse email valide!';
                 return;
             }
             if (this.formData.question.length < 5 || this.formData.message.length < 5) {
-                this.error = 'Question and message need to be at least five symbols!';
+                this.error = 'La question et le message doivent comporter au moins cinq caractères!';
                 return;
             }
 
@@ -32,7 +32,7 @@ function contactForm() {
                     this.formData.email = '';
                     this.formData.question = '';
                     this.formData.message = '';
-                    alert('Thank you! Your question is sucessfully submitted!');
+                    alert('Merci! Votre question a été envoyée avec succès!');
                 })
                 .catch(() => this.error);
         }
@@ -43,9 +43,9 @@ function navBar() {
     return {
         openCloseNav() {
             let sideNavBar = document.getElementById("mySidepanel");
-            sideNavBar.style.width == "500px"
+            sideNavBar.style.width == "450px"
                 ? sideNavBar.style.width = "0"
-                : sideNavBar.style.width = "500px";
+                : sideNavBar.style.width = "450px";
         }
     }
 }
