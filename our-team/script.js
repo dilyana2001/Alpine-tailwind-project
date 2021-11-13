5b22da5a14f3cd7cf3bf297121bed7ccfe78137a
dilyana2001 <dilyana2001@gmail.com>
2021-11-13 15:21:58 +0200

    toggle and for set

---
+++ our-team/script.js
@@ -0,0 +1,7 @@
+// function people() {
+//     return fetch('https://reqres.in/api/users?page=1&per_page=6')
+//         .then(res => res.json())
+//         .then(result => result.data)
+//         .catch((err) => alert(err));
+// }
+
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,51 +1,76 @@
 function contactForm() {
     return {
+        numberSteps: 5,
         formData: {
             email: '',
             question: '',
-            message: ''
+            message: '',
         },
-        error: '',
-        submitDataHandler() {
-            this.error = '';
+        error: [],
+        message: '',
+        errorHandler() {
+            this.error = [];
+            this.message = '';
             if (!this.formData.email || !this.formData.question || !this.formData.message) {
-                this.error = 'Tous les champs sont requis!';
-                return;
+                this.error.push('Tous les champs sont requis!');
             }
-            if (!this.formData.email.match(/[a-z\d]+[.]?[_]?[-]?[a-z\d]+@{1}[a-z]+[-]?[a-z]+[.]?[a-z]+[.][a-z]+/g)) {
-                this.error = 'S\'il vous plaît, mettez une adresse email valide!';
-                return;
+            if (!this.formData.email.match(/[a-z\d]+[.]?[_]?[-]?[a-z\d]+@{1}[a-z]+[-]?[a-z]+[.]?[a-z]+[.][a-z]+/)) {
+                this.error.push('S\'il vous plaît, mettez une adresse email valide!');
             }
             if (this.formData.question.length < 5 || this.formData.message.length < 5) {
-                this.error = 'La question et le message doivent comporter au moins cinq caractères!';
-                return;
+                this.error.push('La question et le message doivent comporter au moins cinq caractères!');
             }
-
-            return fetch('https://jsonplaceholder.typicode.com/todos', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(this.formData)
-            })
-                .then(() => {
-                    this.formData.email = '';
-                    this.formData.question = '';
-                    this.formData.message = '';
-                    alert('Merci! Votre question a été envoyée avec succès!');
+            return this.error;
+        },
+        personalData: {
+            name: '',
+            newsletter: false
+        },
+        nameError: [],
+        nameErrorHandler() {
+            this.nameError = [];
+            if (!this.personalData.name) {
+                this.nameError.push('Entrez votre nom.');
+                console.log(this.personalData);
+                return this.nameError;
+            }
+            if (!this.formData.email || !this.formData.question || !this.formData.message) {
+                this.nameError.push('Veuillez appuyer sur le bouton Précédent et remplir tous les champs!');
+                return this.nameError;
+            }
+            this.submitDataHandler();
+        },
+        submitDataHandler() {
+            this.error = [];
+            this.message = '';
+            let data = {
+                ...this.formData,
+                ...this.personalData
+            }
+            if (this.error.length == 0) {
+                return fetch('https://jsonplaceholder.typicode.com/todos', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
                 })
-                .catch(() => this.error);
+                    .then(() => {
+                        this.formData.email = '';
+                        this.formData.question = '';
+                        this.formData.message = '';
+                        this.personalData.name = '',
+                            this.message = 'Merci! Votre question a été envoyée avec succès!';
+                    })
+                    .catch((err) => {
+                        if (err) {
+                            console.log(err);
+                            return this.error = err;
+                        }
+                        return this.error;
+                    });
+            }
         }
     }
 }
 
-function navBar() {
-    return {
-        openCloseNav() {
-            let sideNavBar = document.getElementById("mySidepanel");
-            sideNavBar.style.width == "450px"
-                ? sideNavBar.style.width = "0"
-                : sideNavBar.style.width = "450px";
-        }
-    }
-}
