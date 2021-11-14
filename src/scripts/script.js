f6f35facbeba64f118ee8b1231b8cc0f49bf6cab
dilyana2001 <dilyana2001@gmail.com>
2021-11-14 22:00:13 +0200

    sub-steps implemented

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,77 +1,75 @@
 function contactForm() {
     return {
-        numberSteps: 1,
+        numberSteps: '',
+        message: '',
         formData: {
             email: '',
             question: '',
             message: '',
         },
+        nameData: {
+            name: '',
+            newsletter: ''
+        },
+        arrayNameData: {
+            name: []
+        },
         error: [],
-        message: '',
+        nameError: [],
+
         errorHandler() {
             this.error = [];
             this.message = '';
-            if (!this.formData.email || !this.formData.question || !this.formData.message) {
+            if (!this.formData.email || !this.formData.question || !this.formData.message || !this.numberSteps) {
                 this.error.push('Tous les champs sont requis!');
             }
             if (!this.formData.email.match(/[a-z\d]+[.]?[_]?[-]?[a-z\d]+@{1}[a-z]+[-]?[a-z]+[.]?[a-z]+[.][a-z]+/)) {
-                this.error.push('S\'il vous plaît, mettez une adresse email valide!');
+                this.error.push(`S'il vous plaît, mettez une adresse email valide!`);
             }
             if (this.formData.question.length < 5 || this.formData.message.length < 5) {
                 this.error.push('La question et le message doivent comporter au moins cinq caractères!');
             }
             return this.error;
         },
-        personalData: {
-            name: '',
-            newsletter: false
-        },
-        nameError: [],
-        nameErrorHandler() {
-            this.nameError = [];
-            if (!this.personalData.name) {
-                this.nameError.push('Entrez votre nom.');
-                console.log(this.personalData);
-                return this.nameError;
-            }
-            if (!this.formData.email || !this.formData.question || !this.formData.message) {
-                this.nameError.push('Veuillez appuyer sur le bouton Précédent et remplir tous les champs!');
-                return this.nameError;
-            }
-            this.submitDataHandler();
-        },
-        submitDataHandler() {
-            this.error = [];
-            this.message = '';
-            let data = {
-                ...this.formData,
-                ...this.personalData
-            }
-            if (!this.error.length == 0) {
+
+        nameHandler() {
+            if (this.arrayNameData.name.length > this.numberSteps) {
                 return;
             }
+            this.arrayNameData.name.push([this.nameData.name, this.nameData.newsletter]);
+            this.nameData.name = '';
+            this.nameData.newsletter = false;
+        },
+
+        nameErrorHandler() {
+            this.nameError = [];
+            this.message = '';
+            if (this.arrayNameData.name.length < this.numberSteps) {
+                this.nameError.push('Entrez tous les noms!');
+                return this.nameError;
+            }
+
             return fetch('https://jsonplaceholder.typicode.com/todos', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(data)
+                body: JSON.stringify({ ...this.formData, ...this.arrayNameData })
             })
                 .then(() => {
                     this.formData.email = '';
                     this.formData.question = '';
                     this.formData.message = '';
-                    this.personalData.name = '',
-                        this.message = 'Merci! Votre question a été envoyée avec succès!';
+                    this.numberSteps = '';
+                    this.message = 'Merci! Votre question a été envoyée avec succès!';
                 })
                 .catch((err) => {
                     if (err) {
-                        console.log(err);
-                        return this.error = err;
+                        this.error.push(err);
                     }
                     return this.error;
                 });
-        }
+        },
     }
 }
 
