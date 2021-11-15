c30ca74df3b9ecc02982e8823c6952e5913fdd96
dilyana2001 <dilyana2001@gmail.com>
2021-11-15 16:24:25 +0200

    add swiper

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,7 @@
 function contactForm() {
     return {
-        numberSteps: '',
-        message: '',
+        numberSteps: 4,
+        successMessage: '',
         formData: {
             email: '',
             question: '',
@@ -9,44 +9,55 @@ function contactForm() {
         },
         nameData: {
             name: '',
-            newsletter: ''
+            newsletter: false,
         },
         arrayNameData: {
             name: []
         },
-        error: [],
-        nameError: [],
+        firstStepErrors: [],
+        subStepErrors: [],
+        secondStepErrors: [],
 
-        errorHandler() {
-            this.error = [];
-            this.message = '';
+        firstStepErrorHandler() {
+            this.firstStepErrors = [];
+            this.successMessage = '';
             if (!this.formData.email || !this.formData.question || !this.formData.message || !this.numberSteps) {
-                this.error.push('Tous les champs sont requis!');
+                this.firstStepErrors.push('Tous les champs sont requis!');
             }
             if (!this.formData.email.match(/[a-z\d]+[.]?[_]?[-]?[a-z\d]+@{1}[a-z]+[-]?[a-z]+[.]?[a-z]+[.][a-z]+/)) {
-                this.error.push(`S'il vous plaît, mettez une adresse email valide!`);
+                this.firstStepErrors.push(`S'il vous plaît, mettez une adresse email valide!`);
             }
             if (this.formData.question.length < 5 || this.formData.message.length < 5) {
-                this.error.push('La question et le message doivent comporter au moins cinq caractères!');
+                this.firstStepErrors.push('La question et le message doivent comporter au moins cinq caractères!');
             }
-            return this.error;
+            return this.firstStepErrors;
         },
 
-        nameHandler() {
-            if (this.arrayNameData.name.length > this.numberSteps) {
+        subStepHandler(id) {
+            this.subStepErrors = [];
+            if (!this.nameData.name) {
+                this.subStepErrors.push('Entrez le nom!');
+                return this.subStepErrors;
+            }
+            if (this.nameData.name) {
+                this.subStepErrors = [];
+            }
+            if (this.arrayNameData.name.length > this.numberSteps - 1) {
                 return;
             }
-            this.arrayNameData.name.push([this.nameData.name, this.nameData.newsletter]);
+
+            this.arrayNameData.name.push([id, this.nameData.name, this.nameData.newsletter]);
             this.nameData.name = '';
             this.nameData.newsletter = false;
+            console.log(this.arrayNameData.name);
         },
 
-        nameErrorHandler() {
-            this.nameError = [];
-            this.message = '';
+        sendDataHandler() {
+            this.secondStepErrors = [];
+            this.successMessage = '';
             if (this.arrayNameData.name.length < this.numberSteps) {
-                this.nameError.push('Entrez tous les noms!');
-                return this.nameError;
+                this.secondStepErrors.push('Entrez tous les noms!');
+                return this.secondStepErrors;
             }
 
             return fetch('https://jsonplaceholder.typicode.com/todos', {
@@ -61,13 +72,13 @@ function contactForm() {
                     this.formData.question = '';
                     this.formData.message = '';
                     this.numberSteps = '';
-                    this.message = 'Merci! Votre question a été envoyée avec succès!';
+                    this.successMessage = 'Merci! Votre question a été envoyée avec succès!';
                 })
                 .catch((err) => {
                     if (err) {
-                        this.error.push(err);
+                        this.firstStepErrors.push(err);
                     }
-                    return this.error;
+                    return this.firstStepErrors;
                 });
         },
     }
