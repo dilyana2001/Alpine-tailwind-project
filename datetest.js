edd9de35b91a9206dacb2c2c539167fa30193bda
dilyana2001 <dilyana2001@gmail.com>
2021-11-23 18:53:10 +0200

    Delete datetest.js

--- datetest.js
+++
@@ -1,7 +0,0 @@
-let dateOfBirthArray = [];
-
-let dateValue = '1988-10-29'
-const regex = /(?<year>[\d]{4,})-(?<month>[\d]{2,})-(?<day>[\d]{2,})/;
-
-let match = regex.exec(dateValue)
-console.log(match)
